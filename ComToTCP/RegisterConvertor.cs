1531c49a9cb35072bc2a6e64fb29458804e078b8
AJassim1 <AJassim@me.com>
2016-07-27 00:43:10 +0300

    upload

---
+++ ComToTCP/RegisterConvertor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComToTCP
+{
+    class RegisterConvertor
+    {
+        public static float ToSingle(ushort[] registers)
+        {
+            if (registers.Length > 2) return 0;
+
+            int value = registers[0] << 16;
+            value = value | registers[1];
+            float res = BitConverter.ToSingle(BitConverter.GetBytes(value), 0);
+
+            return res;
+        }
+
+        public static float[] ToSingleArray(ushort[] registers)
+        {
+            float[] res = new float[registers.Length / 2];
+            for (int i = 0; i < registers.Length / 2; i++)
+            { 
+                res[i] = RegisterConvertor.ToSingle(new ushort[] {registers[i*2], registers[i*2+1]});
+            }
+            return res;
+        }
+
+        public static int ToInt(ushort[] registers)
+        {
+            if (registers.Length > 2) return 0;
+
+            int value = registers[0] << 16;
+            value = value | registers[1];
+            int res = BitConverter.ToInt32(BitConverter.GetBytes(value), 0);
+
+            return res;
+        }
+
+        public static int[] ToIntArray(ushort[] registers)
+        {
+            int[] res = new int[registers.Length / 2];
+            for (int i = 0; i < registers.Length / 2; i++)
+            {
+                res[i] = RegisterConvertor.ToInt(new ushort[] { registers[i * 2], registers[i * 2 + 1] });
+            }
+            return res;
+        }
+
+        public static ushort[] GetRegisters(float value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            ushort[] res = new ushort[2];
+            res[0] = BitConverter.ToUInt16(bytes, 2);
+            res[1] = BitConverter.ToUInt16(bytes, 0);
+
+            return res;
+        }
+
+        public static ushort[] GetRegisters(float[] value)
+        {
+            ushort[] res = new ushort[value.Length * 2 + 1];
+            for (int i = 0; i < value.Length; i++)
+            {
+                ushort[] tmp_res = RegisterConvertor.GetRegisters(value[i]);
+                res[i * 2] = tmp_res[0];
+                res[i * 2 + 1] = tmp_res[1];
+            }
+
+            return res;
+        }
+    }
+}
---
+++ ComToTCP/command variable.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+using Modbus;
+using Modbus.Extensions;
+using Modbus.Device;
+
+
+namespace ComToTCP
+{
+    class command_variable
+    {
+    }
+
+    public class GloablClass
+    {
+        public static string IPaddress;
+        public static int connected = 0;
+        public static ModbusIpMaster master;
+        public static TcpClient tcp;
+
+        public static TcpListener listener;
+        public static ModbusSlave slave;
+
+        public static bool WflowMeter = false;
+        public static bool WpressureMeter = false;
+        public static bool OflowMeter = false;
+        public static bool OpressureMeter = false;
+        public static bool MflowMeter = false;
+        public static bool MpressureMeter = false;
+        public static bool A_1FlowMeter = false;
+        public static bool ApressureMeter = false;
+        public static bool A_2FlowMeter = false;
+        public static bool A_3FlowMeter = false;
+           
+        public static bool PingIp(string ipAddress)
+        {
+            Ping ping = new Ping();
+            int counter = 0;
+
+            for (counter = 0; counter < 3; counter++)
+            {
+                PingReply reply = ping.Send(ipAddress);
+                if (reply.Status == IPStatus.TimedOut)
+                {
+                    GloablClass.connected = 0;
+                    MessageBox.Show(ipAddress + " Connectivity Poor!", "Error");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool CreateMaster(string ipAddress)
+        {
+            try
+            {
+                GloablClass.tcp = new TcpClient(ipAddress, 502);
+                GloablClass.master = ModbusIpMaster.CreateIp(tcp);
+                connected = 1;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                connected = 0;
+                MessageBox.Show(ipAddress + " ModBus Connection Wrong!", "Error");
+                return false;
+            }
+        }
+
+        public static bool CreateSlave(byte[] slaveAdderss)
+        {
+            System.Net.IPAddress ip = new System.Net.IPAddress(slaveAdderss);
+            listener = new TcpListener(ip, 502);
+            slave = ModbusTcpSlave.CreateTcp(1, listener);
+            slave.Listen();
+
+            return true;
+        }
+
+
+
+    }
+}
